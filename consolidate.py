49b9e4faecbd12e17090ff18064d46bbfec37ef9
mateogon <mateogon97@hotmail.com>
2023-07-18 22:00:46 -0400

    modifications

--- consolidate.py
+++ consolidate.py
@@ -1,47 +1,66 @@
 import os
 import sys
 from gitignore_parser import parse_gitignore
+import argparse
 
 def copy_code(repo_dir):
+    print(f"Copying code from {repo_dir}")
 
-  print(f"Copying code from {repo_dir}")
+    # Parse .gitignore
+    gitignore = os.path.join(repo_dir, '.gitignore')
+    if os.path.exists(gitignore):
+        matches = parse_gitignore(gitignore)
+    else:
+        print('.gitignore not found, skipping ignore checks')
+        matches = None
 
-  # Parse .gitignore
-  gitignore = os.path.join(repo_dir, '.gitignore')
-  matches = parse_gitignore(gitignore)
+    # Print directory tree
+    combined_code = '\n# Directory structure:\n'
 
-  # Print directory tree
-  combined_code = '\n# Directory structure:\n'
+    for root, dirs, files in os.walk(repo_dir):
 
-  for root, dirs, files in os.walk(repo_dir):
+        # Filter files using matches function
+        files = [f for f in files if not matches(os.path.join(root, f))]
 
-    # Filter files using matches function
-    files = [f for f in files if not matches(os.path.join(root, f))]
+        level = root.replace(repo_dir, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        combined_code += '{}{}/\n'.format(indent, os.path.basename(root))
 
-    level = root.replace(repo_dir, '').count(os.sep) 
-    indent = ' ' * 4 * (level)
-    combined_code += '{}{}/\n'.format(indent, os.path.basename(root))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            combined_code += '{}{}\n'.format(subindent, f)
 
-    subindent = ' ' * 4 * (level + 1)
-    for f in files:
-      combined_code += '{}{}\n'.format(subindent, f)
+    # Copy non-ignored python code
+    for root, dirs, files in os.walk(repo_dir):
+        files = [f for f in files if not matches(os.path.join(root, f))]
 
-  # Copy non-ignored python code
-  for root, dirs, files in os.walk(repo_dir):
+        for file in files:
+            if file.endswith('.py'):
+                file_path = os.path.join(root, file)
+                
+                with open(file_path, 'r') as f:
+                    code = f.read()
+                    combined_code += f"\n#### file: {file_path}\n{code}\n"
 
-    files = [f for f in files if not matches(os.path.join(root, f))]
+    return combined_code
 
-    for file in files:
-      if file.endswith('.py'):
-        file_path = os.path.join(root, file)
-        
-        with open(file_path, 'r') as f:
-          code = f.read()
-          combined_code += f"\n#### file: {file_path}\n{code}\n"
-
-  with open('combined_code.txt', 'w') as f:
-    f.write(combined_code)  
 
 if __name__ == '__main__':
-  repo_dir = sys.argv[1]
-  copy_code(repo_dir)
+    parser = argparse.ArgumentParser(description='Consolidate repository code into a text file.')
+    parser.add_argument('repo_dir', help='Path to the repository directory')
+    args = parser.parse_args()
+
+    repo_dir = args.repo_dir
+    if not os.path.isdir(repo_dir):
+        sys.exit("Error: The specified repository directory does not exist.")
+
+    try:
+        combined_code = copy_code(repo_dir)
+        output_file = 'combined_code.txt'
+
+        with open(output_file, 'w') as f:
+            f.write(combined_code)
+
+        print(f"Code copied and saved to {output_file} successfully.")
+    except Exception as e:
+        sys.exit(f"An error occurred while copying the code: {str(e)}")
--- readme.md
+++ readme.md
@@ -18,53 +18,57 @@ This will generate a `combined_code.txt` file with all code concatenated.
 
 ## How it works
 
-The main steps are:
+The main steps involved in the script are as follows:
 
 ### Parse .gitignore
 
-Use `gitignore_parser` to get ignoring matching function.
+The script checks if a `.gitignore` file exists in the repository directory. If found, it uses the `gitignore_parser` library to parse the `.gitignore` file and obtain the ignoring matching function.
 
 ```python
 gitignore = os.path.join(repo_dir, '.gitignore')
-matches = parse_gitignore(gitignore)
+if os.path.exists(gitignore):
+    matches = parse_gitignore(gitignore)
+else:
+    print('.gitignore not found, skipping ignore checks')
+    matches = None
 ```
 
 ### Walk directory tree
 
-Recursively walk repository directory using `os.walk()`.
+The script recursively walks through the repository directory using `os.walk()` to traverse all subdirectories and files. It filters out the ignored files based on the matching function obtained from the `.gitignore` file.
 
 ```python
 for root, dirs, files in os.walk(repo_dir):
-
-  # Filter ignored files
-  files = [f for f in files if not matches(os.path.join(root, f))]
+    # Filter ignored files
+    files = [f for f in files if not matches(os.path.join(root, f))]
 ```
 
 ### Copy Python code
 
-For each `.py` file, read contents and append to output.
+For each file, the script checks if it is a Python file (ends with `.py`). If it is, the script reads the contents of the file and appends it to the output string.
 
 ```python
 for file in files:
-
-  if file.endswith('.py'):
-
-    with open(file) as f:
-      code = f.read()
-      combined_code += f"\n# {file_path}\n{code}\n"
+    if file.endswith('.py'):
+        file_path = os.path.join(root, file)
+        with open(file_path, 'r') as f:
+            code = f.read()
+            combined_code += f"\n#### file: {file_path}\n{code}\n"
 ```
 
 ### Write output file
 
-Finally, write concatenated code string to `combined_code.txt`.
+Finally, the script writes the concatenated code string to the `combined_code.txt` file.
 
 ```python
-with open('combined_code.txt', 'w') as f:
-  f.write(combined_code)
+output_file = 'combined_code.txt'
+
+with open(output_file, 'w') as f:
+    f.write(combined_code)
 ```
 
 ## License
 
 This code is released under the MIT License.
 
-Let me know if you would like me to modify or expand the README content further.
+I apologize for the confusion earlier, and thank you for bringing it to my attention. The updated README now accurately reflects the steps performed by the code.
